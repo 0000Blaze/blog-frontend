cf6e82954181574623d36a51f37856d35c113a9b
RohanChhetry <47933141+0000Blaze@users.noreply.github.com>
2022-06-12 16:10:41 +0545

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://rohanpersonalblog.herokuapp.com/api/",
+    baseURL: "https://blazeblog.herokuapp.com/",
 });
